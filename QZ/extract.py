0f9ae02bd596805ce165be68a4753746e4f1b975
qzhu2017 <alecfans@gmail.com>
2025-12-08 12:31:49 -0500

    update extract.py for debugging purpose

--- QZ/extract.py
+++ QZ/extract.py
@@ -4,12 +4,13 @@ from pyxtal import pyxtal
 from pyxtal.util import new_struc_wo_energy
 
 df = pd.read_csv('electride_analysis.csv')
-db = database_topology('electride_data.db')
+db = database_topology('electride_data_18k.db')
 db1 = database_topology('final.db')
 xtals = db1.get_all_xtals()
 
 for row in db.db.select():
     id = row.id
+    if id == 2000: break
     df_row = df[df['formula'] == row.structure_id]
     if len(df_row) > 0:
         csv_row = df_row.iloc[0]
@@ -18,18 +19,14 @@ for row in db.db.select():
         if max(csv_row['band0'], csv_row['band1']) < 20:
             continue
 
+        add = False
         xtal = pyxtal()
-        for tol in [5e-2, 1e-2, 1e-3, 1e-4]:
-            #print(id, tol)
-            atoms = row.toatoms()
-            try:
-                xtal.from_seed(atoms, tol=tol)
-            except:
-                print('Error in spg determiation', tol)
-                continue
-
-            if not xtal.valid: continue
-
+        for tol in [5e-2, 1e-2, 1e-3, 1e-4, 1e-5]:
+            atoms = row.toatoms()#; atoms.write('tmp.vasp', vasp5=True, format='vasp')
+            xtal.from_seed(atoms, tol=tol)
+            add = True
+            if not xtal.valid or xtal.source is None: continue
+            #print(xtal, id, tol)
             dicts = {}
             for field in ['e0025', 'e05', 'e10', 'band0', 'band1', 'e_above_hull']:
                 if field in csv_row.index:
@@ -41,3 +38,7 @@ for row in db.db.select():
             else:
                 print('duplicate:', row.structure_id, tol, row.id)
             break
+
+        if not add:
+            atoms.write('tmp.vasp', vasp5=True, direct=True, format='vasp')
+            print('Error in spg determiation, check tmp.vasp', tol); import sys; sys.exit()
